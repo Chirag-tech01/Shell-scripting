f4255e7f674a06e88841ef0330d037e986610b28
Chirag Barotia <you@example.com>
2024-06-21 16:38:50 +0530

    File handling Assigments

---
+++ CountLines.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+count() {
+    local filename=$1
+    
+    if [ -f "$filename" ]; then
+        local line_count=$(wc -l < "$filename")
+        echo "The file '$filename' has $line_count lines."
+    else
+        echo "File '$filename' not found."
+    fi
+}
+
+# Calling the function 
+count "myfile.txt"
+count "anotherfile.txt"
+count "nonexistentfile.txt"
